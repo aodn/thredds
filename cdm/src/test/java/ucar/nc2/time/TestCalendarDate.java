dd8a69cf9c3a9a627958bdc002a58fa9d42b97cf
Craig Jones <Craig.Jones@utas.edu.au>
2014-08-25 16:06:24 +1000

    Calculate closest date/time to raw value

--- cdm/src/test/java/ucar/nc2/time/TestCalendarDate.java
+++ cdm/src/test/java/ucar/nc2/time/TestCalendarDate.java
@@ -1,6 +1,10 @@
 package ucar.nc2.time;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import ucar.nc2.time.CalendarPeriod.Field;
 
 /**
  * CalendarDate testing
@@ -34,6 +38,27 @@ public class TestCalendarDate {
     cd = CalendarDate.of(null, 1,1,1,0,1,1);
     cd = CalendarDate.of(null, 1,1,1,1,0,1);
     cd = CalendarDate.of(null, 1,1,1,1,1,0);
+  }
+
+  @Test
+  public void testAddReturnsClosestDate() {
+    String baseDate = "1950-01-01";
+    double valueInMillisecs = 2025829799999.99977;
+    String expectedResult = "2014-03-13T02:30:00Z";
+    
+    assertAddReturnsExpectedDate(baseDate, valueInMillisecs, Field.Millisec, expectedResult);
+    assertAddReturnsExpectedDate(baseDate, valueInMillisecs/CalendarDate.MILLISECS_IN_SECOND, Field.Second, expectedResult);
+    assertAddReturnsExpectedDate(baseDate, valueInMillisecs/CalendarDate.MILLISECS_IN_MINUTE, Field.Minute, expectedResult);
+    assertAddReturnsExpectedDate(baseDate, valueInMillisecs/CalendarDate.MILLISECS_IN_HOUR, Field.Hour, expectedResult);
+    assertAddReturnsExpectedDate(baseDate, valueInMillisecs/CalendarDate.MILLISECS_IN_DAY, Field.Day, expectedResult);
+    assertAddReturnsExpectedDate(baseDate, valueInMillisecs/CalendarDate.MILLISECS_IN_MONTH, Field.Month, expectedResult);
+    assertAddReturnsExpectedDate(baseDate, valueInMillisecs/CalendarDate.MILLISECS_IN_YEAR, Field.Year, expectedResult);
+  }
+  
+  private void assertAddReturnsExpectedDate(String baseDate, double value, Field units, String expectedResult) {
+    CalendarDate base = CalendarDateFormatter.isoStringToCalendarDate(Calendar.gregorian, baseDate);
+    CalendarDate result = base.add(value, units);
+    assertEquals(units.toString(), expectedResult, CalendarDateFormatter.toDateTimeStringISO(result));
   }
 
 }
