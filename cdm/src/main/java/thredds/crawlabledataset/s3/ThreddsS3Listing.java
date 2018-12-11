00113bdc3d9128c0626f51e58cf522b678a4b3ce
Alex McKeown <a.mckeown@utas.edu.au>
2018-12-11 13:47:40 +1100

    Fixy Fix

--- cdm/src/main/java/thredds/crawlabledataset/s3/ThreddsS3Listing.java
+++ cdm/src/main/java/thredds/crawlabledataset/s3/ThreddsS3Listing.java
@@ -2,10 +2,14 @@ package thredds.crawlabledataset.s3;
 
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * Listing of the "virtual directory" at a specified Amazon S3 URI.
@@ -14,6 +18,8 @@ import java.util.Objects;
  * @since 2016/09/12
  */
 public class ThreddsS3Listing {
+    private static final Logger logger = LoggerFactory.getLogger(ThreddsS3Listing.class);
+
     private final S3URI s3uri;
 
     private List<ThreddsS3Metadata> entries = new ArrayList<>();
@@ -31,14 +37,31 @@ public class ThreddsS3Listing {
             return;
         }
 
-        for (String commonPrefix : objectListing.getCommonPrefixes()) {
-            entries.add(new ThreddsS3Directory(new S3URI(s3uri.getBucket(), commonPrefix)));
+        List<String> legitFiles = new ArrayList<>();
+        for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+            String [] knownFileExtensions = {".hdf", ".xml", ".nc", ".bz2", ".cdp", ".jpg"};
+
+            for(int i=0;i<knownFileExtensions.length;i++) {
+                if (objectSummary.getKey().endsWith(knownFileExtensions[i])) {
+                    legitFiles.add(objectSummary.getKey());
+                    entries.add(new ThreddsS3Object(objectSummary));
+                }
+            }
         }
 
-        for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
-            if(objectSummary.getKey().contains(".nc")) {
-                entries.add(new ThreddsS3Object(objectSummary));
+        List<String> commonPrefixes = objectListing.getCommonPrefixes();
+        Set<String> legitCommonPrefixes = new HashSet<>();
+        for(String file : legitFiles) {
+            for(String prefix : commonPrefixes)  {
+                if(file.startsWith(prefix)) {
+                    legitCommonPrefixes.add(prefix);
+                }
             }
+        }
+
+        for (String commonPrefix : legitCommonPrefixes) {
+            logger.info("COMMON PREFIX: " + commonPrefix);
+            entries.add(new ThreddsS3Directory(new S3URI(s3uri.getBucket(), commonPrefix)));
         }
     }
 
